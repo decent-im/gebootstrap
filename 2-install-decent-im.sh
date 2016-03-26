ea5c99d152a3600c30e52d19afc2d2e1d5eb2d5d
Andrey Utkin <andrey_utkin@fastmail.com>
2016-03-26 21:03:49 +0200

    Drop FQDN detection, code is moved to package's util

--- 2-install-decent-im.sh
+++ 2-install-decent-im.sh
@@ -15,25 +15,6 @@ ln -snfv /var/lib/layman/decent-im/profiles/hardened/linux/amd64/no-multilib /et
 emerge --sync
 emerge -vuDN @world
 
-# Make sure `hostname -f` shows what you want to be your FQDN! decent-im generates configs based on that
-EXTERNAL_IP=`dig +short myip.opendns.com @resolver1.opendns.com`
-DNS_PTR=`dig +short -x $EXTERNAL_IP @resolver1.opendns.com`
-FQDN=`echo $DNS_PTR | sed 's/[.]$//'`
-echo "$FQDN" > /etc/fqdn
-HOSTNAME=`echo $FQDN | sed 's/[.].*$//'`
-DOMAIN=`echo $FQDN | sed 's/^[^.]*[.]//'`
-echo "$HOSTNAME" > /etc/hostname
-hostname $HOSTNAME
-
-echo "Detected external IP: $EXTERNAL_IP"
-echo "Detected FQDN: $FQDN"
-echo "System-visible FQDN: `hostname -f`"
-
-# Ensure no other domain present
-sed -i /etc/resolv.conf -e 's/^domain.*$//'
-echo "domain $DOMAIN" >> /etc/resolv.conf
-echo "domain $DOMAIN" > /etc/resolv.conf.head
-
 # Workaround until all lua packages support LUA_TARGETS
 emerge -v dev-lang/lua:5.1
 eselect lua set 5.1
