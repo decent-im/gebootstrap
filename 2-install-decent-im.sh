fd6422de2da9b504b964170d1297c2eef609c822
Andrey Utkin <andrey_utkin@fastmail.com>
2017-11-29 01:23:02 +0000

    Update recommended make.profile setting

--- 2-install-decent-im.sh
+++ 2-install-decent-im.sh
@@ -10,7 +10,12 @@ layman -a dev-zero
 yes | layman -o https://raw.githubusercontent.com/decent-im/gentoo-overlay/master/layman.xml -f -a decent-im
 
 # Switch to custom profile
-ln -snfv /var/lib/layman/decent-im/profiles/hardened/linux/amd64/no-multilib /etc/portage/make.profile
+rm -f /etc/portage/make.profile
+mkdir /etc/portage/make.profile
+cat > /etc/portage/make.profile/parent <<EOF
+/usr/portage/profiles/default/linux/amd64/17.0/no-multilib
+/var/lib/layman/decent-im/profiles/features/decent.im
+EOF
 
 # Let profile changes take effect (e.g. un-bindist openssl)
 emerge --sync
